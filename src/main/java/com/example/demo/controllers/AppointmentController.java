97d8c39641050f94e306cd2fbb30edc9e808fda5
Franmb93 <fcomunozbetanzos@gmail.com>
2024-02-26 21:33:43 +0100

    Step 1. Developed the appointment creation endpoint.

--- src/main/java/com/example/demo/controllers/AppointmentController.java
+++ src/main/java/com/example/demo/controllers/AppointmentController.java
@@ -27,12 +27,12 @@ public class AppointmentController {
     AppointmentRepository appointmentRepository;
 
     @GetMapping("/appointments")
-    public ResponseEntity<List<Appointment>> getAllAppointments(){
+    public ResponseEntity<List<Appointment>> getAllAppointments() {
         List<Appointment> appointments = new ArrayList<>();
 
         appointmentRepository.findAll().forEach(appointments::add);
 
-        if (appointments.isEmpty()){
+        if (appointments.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NO_CONTENT);
         }
 
@@ -40,43 +40,67 @@ public class AppointmentController {
     }
 
     @GetMapping("/appointments/{id}")
-    public ResponseEntity<Appointment> getAppointmentById(@PathVariable("id") long id){
+    public ResponseEntity<Appointment> getAppointmentById(@PathVariable("id") long id) {
         Optional<Appointment> appointment = appointmentRepository.findById(id);
 
-        if (appointment.isPresent()){
-            return new ResponseEntity<>(appointment.get(),HttpStatus.OK);
-        }else {
+        if (appointment.isPresent()) {
+            return new ResponseEntity<>(appointment.get(), HttpStatus.OK);
+        } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
 
+    /**
+     * Validates the appointment received,
+     * checks if the appointment overlap with any other appointment
+     * and if two conditions are fine, the appointment is created
+     *
+     * @param appointment Appointment received through the endpoint .
+     * @return ResponseEntity with the appointmentList with the new appointment added and an HTTP OK response if successful.
+     * ResponseEntity with HTTP NOT_ACCEPTABLE if appointment is overlying with another one.
+     * ResponseEntity with HTTP BAD_REQUEST if the appointment doesn't go through the validations.
+     */
     @PostMapping("/appointment")
-    public ResponseEntity<List<Appointment>> createAppointment(@RequestBody Appointment appointment){
-        /** TODO 
-         * Implement this function, which acts as the POST /api/appointment endpoint.
-         * Make sure to check out the whole project. Specially the Appointment.java class
-         */
-        return new ResponseEntity<>(HttpStatus.I_AM_A_TEAPOT);
+    public ResponseEntity<List<Appointment>> createAppointment(@RequestBody Appointment appointment) {
+        if (appointment.getStartsAt() == null || appointment.getFinishesAt() == null || appointment.getStartsAt().isAfter(appointment.getFinishesAt()) || appointment.getStartsAt().isEqual(appointment.getFinishesAt())) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        List<Appointment> appointmentsList = appointmentRepository.findAll();
+
+        // NOTE: I do not consider this to be efficient in a database with many appointments. The ideal would be to first extract the logic to a service,
+        // query the database and treat this as a batch, fetching the appointments as you iterate through an ordered list and only until an overlap is encountered
+        // But this cannot be done due to limitations in editing files other than this one and the test files.
+        for (Appointment existingAppointment : appointmentsList) {
+            if (appointment.overlaps(existingAppointment)) {
+                return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+            }
+        }
+
+        Appointment savedAppointment = appointmentRepository.save(appointment);
+        appointmentsList.add(savedAppointment);
+
+        return new ResponseEntity<>(appointmentsList, HttpStatus.OK);
     }
 
 
     @DeleteMapping("/appointments/{id}")
-    public ResponseEntity<HttpStatus> deleteAppointment(@PathVariable("id") long id){
+    public ResponseEntity<HttpStatus> deleteAppointment(@PathVariable("id") long id) {
 
         Optional<Appointment> appointment = appointmentRepository.findById(id);
 
-        if (!appointment.isPresent()){
+        if (!appointment.isPresent()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
 
         appointmentRepository.deleteById(id);
 
         return new ResponseEntity<>(HttpStatus.OK);
-        
+
     }
 
     @DeleteMapping("/appointments")
-    public ResponseEntity<HttpStatus> deleteAllAppointments(){
+    public ResponseEntity<HttpStatus> deleteAllAppointments() {
         appointmentRepository.deleteAll();
         return new ResponseEntity<>(HttpStatus.OK);
     }
